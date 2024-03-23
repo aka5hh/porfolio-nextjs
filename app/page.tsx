15881452f3f86b0a62c9b6127923a500644c0300
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2024-03-23 23:58:50 +0600

    social midea added

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,122 @@ export default function Home() {
       <div className="items-center space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
         <div className="flex flex-col items-center pt-8 space-x-2">
 
-          <Image alt="Picture of Abu Bakar Siddique" src={Me} 
-          className="h-48 w-48 rounded-full object-cover object-top" />
+          <Image alt="Picture of Abu Bakar Siddique" src={Me}
+            className="h-48 w-48 rounded-full object-cover object-top" />
           <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
-            Abu Bakar Siddique</h3>
+            Abu Bakar Siddique
+          </h3>
+          <p className="text-gray-500 dark:text-gray-300 text-center">
+            Hey my name is Abu Bakar , I'm a Website Developer/Software Engineer.
+          </p>
+
+          <div className="flex space-x-5 pt-6">
+
+            {/* <li className="m-1 inline-block">
+              <a className="h-8 w-8 text-base flex items-center justify-center rounded-full border-2 border-primary 
+              text-secondary-1 transition-all duration-300 ease-out hover:bg-primary
+              hover:text-slate-500 dark:hover:bg-slate-400 hover:shadow-lg active:scale-95"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/abubakarsiddiquee/">
+                <svg stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 448 512"
+                  height="1em" width="1em"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 
+                29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 
+                76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
+                </svg>
+              </a>
+            </li>
+
+
+            <li className="m-1 inline-block">
+              <a className="h-8 w-8 text-base flex items-center justify-center rounded-full border-2 
+                  border-primary text-secondary-1 transition-all duration-300 ease-out hover:bg-primary 
+                  hover:text-slate-500 dark:hover:bg-slate-400 hover:shadow-lg 
+                  active:scale-95"
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/aka5hh">
+                <svg stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 496 512"
+                  height="1em" width="1em"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
+                  </path>
+                </svg>
+              </a>
+            </li>
+
+            <li className="m-1 inline-block">
+              <a className="h-8 w-8 text-base flex items-center justify-center rounded-full border-2 border-primary text-secondary-1 
+              transition-all duration-300 ease-out hover:bg-primary hover:text-slate-500 dark:hover:bg-slate-400 hover:shadow-lg 
+              active:scale-95" target="_blank" rel="noreferrer" 
+              href="mailto:mohammad.akash31@gmail.com">
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  height="1em"
+                  width="1em"                 
+                >
+                  <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 010 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" />
+                </svg>
+              </a>
+            </li> */}
+
+            <a href="https://github.com/aka5hh" target="_blank">
+              <svg 
+              fill="none" 
+              viewBox="0 0 15 15" 
+              className="w-8 h-8 text-teal-500 hover:text-teal-600"             
+              >
+                <path
+                  fill="currentColor"
+                  d="M9.358 2.145a8.209 8.209 0 00-3.716 0c-.706-.433-1.245-.632-1.637-.716a2.17 2.17 0 00-.51-.053 1.258 1.258 0 00-.232.028l-.01.002-.004.002h-.003l.137.481-.137-.48a.5.5 0 00-.32.276 3.12 3.12 0 00-.159 2.101A3.354 3.354 0 002 5.93c0 1.553.458 2.597 1.239 3.268.547.47 1.211.72 1.877.863-.11.305-.138.628-.116.958v.598c-.407.085-.689.058-.89-.008-.251-.083-.444-.25-.629-.49a4.798 4.798 0 01-.27-.402l-.057-.093a9.216 9.216 0 00-.224-.354c-.19-.281-.472-.633-.928-.753l-.484-.127-.254.968.484.127c.08.02.184.095.355.346.063.093.122.19.19.302l.068.11c.094.152.202.32.327.484.253.33.598.663 1.11.832.35.116.748.144 1.202.074V14.5a.5.5 0 00.5.5h4a.5.5 0 00.5-.5v-3.563c0-.315-.014-.604-.103-.873.663-.14 1.322-.39 1.866-.86.78-.676 1.237-1.73 1.237-3.292v-.001a3.354 3.354 0 00-.768-2.125 3.12 3.12 0 00-.159-2.1.5.5 0 00-.319-.277l-.137.48c.137-.48.136-.48.135-.48l-.002-.001-.004-.002-.009-.002a.671.671 0 00-.075-.015 1.261 1.261 0 00-.158-.013 2.172 2.172 0 00-.51.053c-.391.084-.93.283-1.636.716z"
+                />
+              </svg>
+            </a> 
+            
+            <a href="https://www.linkedin.com/in/abubakarsiddiquee/" target="_blank">
+              <svg
+                viewBox="0 0 900 1000"
+                fill="currentColor"
+                className="w-8 h-8 text-teal-500 hover:text-teal-600"
+              >
+                <path d="M204 152c0 26.667-9.667 49.333-29 68s-44.333 28-75 28c-29.333 0-53.333-9.333-72-28S0 178.667 0 152c0-28 9.333-51 28-69s43.333-27 74-27 55 9 73 27 27.667 41 29 69M6 942V324h192v618H6m306-420c0-57.333-1.333-123.333-4-198h166l10 86h4c40-66.667 103.333-100 190-100 66.667 0 120.333 22.333 161 67s61 111 61 199v366H708V600c0-89.333-32.667-134-98-134-46.667 0-79.333 24-98 72-4 8-6 24-6 48v356H312V522" />
+              </svg>
+            </a> 
+            
+            <a href="mailto:mohammad.akash31@gmail.com" target="_blank">
+              <svg
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-8 h-8 text-teal-500 hover:text-teal-600"
+              >
+                <path d="M18.73 5.41l-1.28 1L12 10.46 6.55 6.37l-1.28-1A2 2 0 002 7.05v11.59A1.36 1.36 0 003.36 20h3.19v-7.72L12 16.37l5.45-4.09V20h3.19A1.36 1.36 0 0022 18.64V7.05a2 2 0 00-3.27-1.64z" />
+              </svg>
+            </a>
+
+            <a href="https://twitter.com/m0hammad_akash" target="_blank">
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="currentColor"
+                className="w-8 h-8 text-teal-500 hover:text-teal-600"
+              >
+                <path d="M928 254.3c-30.6 13.2-63.9 22.7-98.2 26.4a170.1 170.1 0 0075-94 336.64 336.64 0 01-108.2 41.2A170.1 170.1 0 00672 174c-94.5 0-170.5 76.6-170.5 170.6 0 13.2 1.6 26.4 4.2 39.1-141.5-7.4-267.7-75-351.6-178.5a169.32 169.32 0 00-23.2 86.1c0 59.2 30.1 111.4 76 142.1a172 172 0 01-77.1-21.7v2.1c0 82.9 58.6 151.6 136.7 167.4a180.6 180.6 0 01-44.9 5.8c-11.1 0-21.6-1.1-32.2-2.6C211 652 273.9 701.1 348.8 702.7c-58.6 45.9-132 72.9-211.7 72.9-14.3 0-27.5-.5-41.2-2.1C171.5 822 261.2 850 357.8 850 671.4 850 843 590.2 843 364.7c0-7.4 0-14.8-.5-22.2 33.2-24.3 62.3-54.4 85.5-88.2z" />
+              </svg>
+            </a>
+
+
+
+          </div>
 
         </div>
-
       </div>
     </div>
 
